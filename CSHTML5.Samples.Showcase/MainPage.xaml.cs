d36ccba3b90556927af09fc925ddae8677e6974f
Giovanni <Giovanni@WIN8-MPB17>
2018-02-10 09:41:30 +0100

    Add project files.

---
+++ CSHTML5.Samples.Showcase/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CSHTML5.Samples.Showcase
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+        }
+    }
+}
