e2afde320e46749aaee61dc03805ce27fa7e32e0
Sylvain <t0ast@hotmail.fr>
2019-09-12 15:27:48 +0200

    Add Slider Demo.

---
+++ CSHTML5.Samples.Showcase/Samples/XAML_Controls/Slider/Slider_Demo_More.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace CSHTML5.Samples.Showcase.Samples.XAML_Controls.Slider
+{
+    public partial class Slider_Demo_More : UserControl
+    {
+        public Slider_Demo_More()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
