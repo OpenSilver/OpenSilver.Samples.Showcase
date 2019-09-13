8ab908806c9fbeffae7ed22cdc46ed2be9fac0c2
Sylvain <t0ast@hotmail.fr>
2019-09-13 17:49:29 +0200

    Add a JSReplacement for InteropHelper.Unbox.

--- CSHTML5.Samples.Showcase/Samples/Third_Party/Plotly_Charts/ChartControl/InteropHelper.cs
+++ CSHTML5.Samples.Showcase/Samples/Third_Party/Plotly_Charts/ChartControl/InteropHelper.cs
@@ -8,7 +8,11 @@ namespace CSHTML5.Extensions.Plotly
 {
     public static class InteropHelper
     {
+#if !BRIDGE
+        [JSIL.Meta.JSReplacement("$value")]
+#else
         [Bridge.Template("({value}.v != undefined ? {value}.v : {value})")]
+#endif
         public static object Unbox(object value) //move this to a helper class "InteropHelper.Unbox(..)" with the same Namespace as plotly.
         {
             return value;
