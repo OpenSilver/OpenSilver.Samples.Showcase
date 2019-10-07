0d1c1f06233b7009398128029e9e11f829fe8c1b
cshtml5 <contact@cshtml5.com>
2019-10-07 18:50:03 +0200

    Add sample for "Attached Properties" and for the "MethodToUpdateDom" property of dependency properties and attached properties.

---
+++ CSHTML5.Samples.Showcase/Samples/XAML_Features/AttachedProperties/AttachedProperties_Demo.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace CSHTML5.Samples.Showcase
+{
+    public partial class AttachedProperties_Demo : UserControl
+    {
+        public AttachedProperties_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonViewSource_Click(object sender, RoutedEventArgs e)
+        {
+            ViewSourceButtonHelper.ViewSource(new List<ViewSourceButtonInfo>()
+            {
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "SampleAttachedProperties.cs",
+                    FilePathOnGitHub = "github/cshtml5/CSHTML5.Samples.Showcase/blob/master/CSHTML5.Samples.Showcase/Samples/XAML_Features/AttachedProperties/SampleAttachedProperties.cs"
+                },
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "AttachedProperties_Demo.xaml",
+                    FilePathOnGitHub = "github/cshtml5/CSHTML5.Samples.Showcase/blob/master/CSHTML5.Samples.Showcase/Samples/XAML_Features/AttachedProperties/AttachedProperties_Demo.xaml"
+                },
+                new ViewSourceButtonInfo()
+                {
+                    TabHeader = "AttachedProperties_Demo.xaml.cs",
+                    FilePathOnGitHub = "github/cshtml5/CSHTML5.Samples.Showcase/blob/master/CSHTML5.Samples.Showcase/Samples/XAML_Features/AttachedProperties/AttachedProperties_Demo.xaml.cs"
+                }
+            });
+        }
+
+    }
+}
