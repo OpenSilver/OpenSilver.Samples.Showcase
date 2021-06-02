8cd95dc97a80f4f86b1be2d2b24bbe9a36914da6
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-06-02 13:53:48 +0200

    Updated .Browser project to .NET 5

---
+++ OpenSilver.Samples.Showcase.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using DotNetForHtml5;
+using OpenSilver.Samples.Showcase.Browser.Interop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+
+namespace OpenSilver.Samples.Showcase.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize((IJavaScriptExecutionHandler)new UnmarshalledJavaScriptExecutionHandler(this.JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
