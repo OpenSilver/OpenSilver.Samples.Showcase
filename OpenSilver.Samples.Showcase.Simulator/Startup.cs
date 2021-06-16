3984a1f72bec7dbb08c16987ffc668a46055cc91
Maxime Duriez <maxime.duriez@cshtml5.com>
2021-06-16 12:55:13 +0200

    Updated .Simulator project to reflect recent simulator handling changes

---
+++ OpenSilver.Samples.Showcase.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilver.Samples.Showcase.Simulator
+{
+    static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start();
+        }
+    }
+}
