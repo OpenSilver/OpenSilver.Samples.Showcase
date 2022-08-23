755db993724a3b40032d23a3bbe0205209f9507b
Pierre Barjon <barjon.pierre@gmail.com>
2022-08-23 17:37:51 +0200

    use opensilver namespace

--- src/Samples/XAML_Features/PropertyChangedTrigger/PropertyChangedTrigger_Demo.xaml.cs
+++ src/Samples/XAML_Features/PropertyChangedTrigger/PropertyChangedTrigger_Demo.xaml.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace CSHTML5.Samples.Showcase
+namespace OpenSilver.Samples.Showcase
 {
     public partial class PropertyChangedTrigger_Demo : UserControl
     {
