8ae205564049e41626b081fd77ff8ca999539c81
Pierre Barjon <barjon.pierre@gmail.com>
2023-03-03 18:43:29 +0100

    Replace MasonryPanels with WrapPanels

---
+++ src/Other/Internals/WrapPanelWithProgressiveLoading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if SLMIGRATION
+using System.Windows.Controls;
+#else
+using Windows.UI.Xaml.Controls;
+#endif
+
+namespace OpenSilver.Samples.Showcase
+{
+    public class WrapPanelWithProgressiveLoading : WrapPanel
+    {
+        public WrapPanelWithProgressiveLoading()
+        {
+            ProgressiveRenderingChunkSize = 3;
+        }
+    }
+}
