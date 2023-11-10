d47b4a5094b3361230c426faac02030f5a3a9acc
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-10 18:13:48 +0100

    Add Virtualization sample + Change the Performance panel to a StaggeredPanel

---
+++ src/Samples/Performance/Virtualization/Virtualization_Demo.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class Virtualization_Demo : UserControl
+    {
+        public List<Tuple<string, string>> colors = GetColorList();
+        public Virtualization_Demo()
+        {
+            this.InitializeComponent();
+            this.DataContext = colors;
+        }
+
+        static List<Tuple<string, string>> GetColorList()
+        {
+            List<Tuple<string, string>> colorList = new List<Tuple<string, string>>();
+
+            // Get all predefined colors
+            foreach (PropertyInfo property in typeof(Colors).GetProperties())
+            {
+                if (property.PropertyType == typeof(Color))
+                {
+                    Color color = (Color)property.GetValue(null, null);
+                    string colorName = property.Name;
+                    string colorCode = "#" + color.ToString().Substring(3); // Convert to hex format
+
+                    colorList.Add(Tuple.Create(colorName, colorCode));
+                }
+            }
+
+            return colorList;
+        }
+
+
+    }
+}
