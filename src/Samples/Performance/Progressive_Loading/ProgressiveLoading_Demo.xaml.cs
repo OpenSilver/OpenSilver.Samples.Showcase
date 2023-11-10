120ded063588657ce80d8626051a4476ac0a8724
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-10 18:14:44 +0100

    Add ProgressiveLoading Sample

---
+++ src/Samples/Performance/Progressive_Loading/ProgressiveLoading_Demo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class ProgressiveLoading_Demo : UserControl
+    {
+        public ProgressiveLoading_Demo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
