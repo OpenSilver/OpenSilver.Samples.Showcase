9c56920b1ccddfec4b8212203fa3da7299179598
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-13 17:48:18 +0100

    Add DataForm Sample

---
+++ src/Samples/XAML_Controls/DataForm/DataForm_Demo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class DataForm_Demo : UserControl
+    {
+        public DataForm_Demo()
+        {
+            this.InitializeComponent();
+
+            // Populate the data form with the list of planets:
+            DataForm1.ItemsSource = Planet.GetListOfPlanets();
+        }
+    }
+}
