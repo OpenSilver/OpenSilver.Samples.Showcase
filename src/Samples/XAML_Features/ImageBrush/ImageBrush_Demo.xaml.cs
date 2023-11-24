1ad7b6e11eb17521af05ed054f1cd7a6b129d454
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-24 15:54:55 +0100

    Add ImageBrush Sample

---
+++ src/Samples/XAML_Features/ImageBrush/ImageBrush_Demo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class ImageBrush_Demo : UserControl
+    {
+        public ImageBrush_Demo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
