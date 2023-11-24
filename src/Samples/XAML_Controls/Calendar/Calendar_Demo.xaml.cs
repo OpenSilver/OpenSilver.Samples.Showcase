01a56f9f2884823e34f66f037fe3a8461556e155
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-24 16:17:21 +0100

    Add Calendar Sample

---
+++ src/Samples/XAML_Controls/Calendar/Calendar_Demo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class Calendar_Demo : UserControl
+    {
+        public Calendar_Demo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
