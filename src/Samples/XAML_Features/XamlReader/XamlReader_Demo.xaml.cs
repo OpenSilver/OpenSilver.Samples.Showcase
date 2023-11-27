61f9b55f057a99bc2b5622763d7b66d9f9aaff31
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-27 16:29:36 +0100

    Add XamlReader Sample

---
+++ src/Samples/XAML_Features/XamlReader/XamlReader_Demo.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class XamlReader_Demo : UserControl
+    {
+        public XamlReader_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonAddFields_Click(object sender, RoutedEventArgs e)
+        {
+            string xaml =
+@"<Grid xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" Margin=""5"">
+                        <Grid.ColumnDefinitions>
+                            <ColumnDefinition Width=""*""/>
+                            <ColumnDefinition Width=""20""/>
+                            <ColumnDefinition Width=""*""/>
+                        </Grid.ColumnDefinitions>
+                        <Grid.RowDefinitions>
+                            <RowDefinition Height=""Auto""/>
+                            <RowDefinition Height=""5""/>
+                            <RowDefinition Height=""Auto""/>
+                        </Grid.RowDefinitions>
+                        <TextBlock Text=""First name:"" VerticalAlignment = ""Center""/>
+                        <TextBox Text=""John"" Grid.Column = ""2"" />
+                        <TextBlock Text=""Last name:"" Grid.Row=""2"" VerticalAlignment=""Center""/>
+                        <TextBox Text=""Doe"" Grid.Row=""2"" Grid.Column=""2""/>
+                    </Grid>";
+            object newContent = XamlReader.Load(xaml);
+            //stackPanelRoot is the visual root of a Page in existing XAML markup already loaded by the appmodel
+            Container.Children.Add(newContent as UIElement);
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            Container.Children.Clear();
+        }
+    }
+}
