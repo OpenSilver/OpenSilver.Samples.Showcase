71be457a190ec5a1c51263d80447a02dadfd44ac
Sylvain Maillard <maillard.sy@gmail.com>
2023-11-27 15:36:36 +0100

    Add Sample for WriteableBitmap

---
+++ src/Samples/Net_Framework/WriteableBitmap/WriteableBitmap_Demo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class WriteableBitmap_Demo : UserControl
+    {
+        public WriteableBitmap_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ClearButton_Click(System.Object sender, System.Windows.RoutedEventArgs e)
+        {
+            ivDestination.Source = null;
+        }
+
+        private async void MirrorButton_Click(System.Object sender, System.Windows.RoutedEventArgs e)
+        {
+            var bitmap = new WriteableBitmap(200, 200);
+            await bitmap.RenderAsync(ivSource, null/* TODO Change to default(_) if this is not a reference type */);
+
+            // Let's modify pixels
+            Array.Reverse(bitmap.Pixels);
+
+            bitmap.Invalidate(); // Invalidate once Pixels are manipulated
+
+            ivDestination.Source = bitmap;
+        }
+    }
+}
