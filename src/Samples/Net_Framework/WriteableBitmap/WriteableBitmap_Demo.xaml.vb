50d3c6e4521bd4ba29d06f02e360a30506217dc4
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 16:09:45 +0100

    Add Vb code for WriteableBitmap Sample

---
+++ src/Samples/Net_Framework/WriteableBitmap/WriteableBitmap_Demo.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Imports System.Windows.Controls
+Imports System.Windows.Media.Imaging
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class WriteableBitmap_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub ClearButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+            ivDestination.Source = Nothing
+        End Sub
+
+        Private Async Sub MirrorButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim bitmap = New WriteableBitmap(200, 200)
+            Await bitmap.RenderAsync(ivSource, Nothing) 'TODO Change to default(_) if this is not a reference type
+            'Let's modify pixels:
+            Array.Reverse(bitmap.Pixels)
+            bitmap.Invalidate() 'Invalidate once Pixels are manipulated:
+            ivDestination.Source = bitmap
+        End Sub
+    End Class
+End Namespace
