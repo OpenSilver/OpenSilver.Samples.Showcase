def54e994c3e90b4f3cfd2c89eb10d7f6246e8c0
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 15:54:57 +0100

    Add VB code for Styles Sample

---
+++ src/Samples/XAML_Features/Styles/Styles_Demo.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Controls
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class Styles_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
