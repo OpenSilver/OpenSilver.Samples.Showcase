c4d0cccbcb826f908787458d9d5040eba88f6e19
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 15:59:11 +0100

    Add VB code for InitParams Sample

---
+++ src/Samples/Net_Framework/InitParams/InitParams_Demo.xaml.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class InitParams_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub ButtonShowInitParams_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim parameters = Application.Current.Host.InitParams
+            Dim initParamsString As String = "I found this in init param:"
+
+            For Each param In parameters
+                initParamsString += $"{Environment.NewLine}key: {param.Key}, value: {param.Value}"
+            Next
+
+            MessageBox.Show(initParamsString)
+        End Sub
+    End Class
+End Namespace
