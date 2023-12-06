5a52e035f63d6ea8ac95f5cacc75bb5444143bf7
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 16:08:46 +0100

    Add VB code for ProgressBar Sample

---
+++ src/Samples/XAML_Controls/ProgressBar/ProgressBar_Demo.xaml.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Windows.Controls
+
+Namespace Global.OpenSilver.Samples.Showcase
+    Partial Public Class ProgressBar_Demo
+        Inherits UserControl
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
