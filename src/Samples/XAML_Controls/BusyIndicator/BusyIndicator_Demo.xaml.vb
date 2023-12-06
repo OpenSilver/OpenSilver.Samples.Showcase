9b922b9e9f4920a7b4101cf123ebc27a3fec92d2
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 15:50:03 +0100

    Add VB code for BusyIndicator Sample

---
+++ src/Samples/XAML_Controls/BusyIndicator/BusyIndicator_Demo.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Threading
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class BusyIndicator_Demo
+        Inherits UserControl
+
+        Private timer As DispatcherTimer
+
+        Public Sub New()
+            Me.InitializeComponent()
+            timer = New DispatcherTimer() With {
+                .Interval = New TimeSpan(0, 0, 3)
+            }
+            AddHandler timer.Tick, AddressOf StopBusyIndicator
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            MyBusyIndicator.IsBusy = True
+            timer.Start()
+        End Sub
+
+        Private Sub StopBusyIndicator(ByVal sender As Object, ByVal e As EventArgs)
+            timer.[Stop]()
+            MyBusyIndicator.IsBusy = False
+        End Sub
+    End Class
+End Namespace
