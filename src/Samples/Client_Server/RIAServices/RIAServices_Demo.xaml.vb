60e3041a871e6c94f70acb04d5ce4f07019af208
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 15:52:09 +0100

    Add VB code for RIAServices Sample

---
+++ src/Samples/Client_Server/RIAServices/RIAServices_Demo.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Controls
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class RIAServices_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
