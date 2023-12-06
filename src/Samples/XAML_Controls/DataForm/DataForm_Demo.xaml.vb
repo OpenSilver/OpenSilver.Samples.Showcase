b27886c6be80fdad530a3ddc79278b11474ffa1b
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-06 15:49:00 +0100

    Add VB code for DataForm Sample

---
+++ src/Samples/XAML_Controls/DataForm/DataForm_Demo.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Controls
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class DataForm_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+            'Populate the data form with the list of planets:
+            DataForm1.ItemsSource = Planet.GetListOfPlanets()
+        End Sub
+    End Class
+End Namespace
