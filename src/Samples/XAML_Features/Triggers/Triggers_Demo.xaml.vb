4463d193804b46e4f513effc297d3728067f21a7
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-13 17:01:28 +0100

    Add Sample for Triggers

---
+++ src/Samples/XAML_Features/Triggers/Triggers_Demo.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Controls
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class Triggers_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
