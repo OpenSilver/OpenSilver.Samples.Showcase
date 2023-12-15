ee3d981bf19e93fc88f51280b0d4a3709899a661
Sylvain Maillard <maillard.sy@gmail.com>
2023-12-15 16:10:33 +0100

    Add Sample for InkPresenter

---
+++ src/Samples/XAML_Controls/InkPresenter/InkPresenter_Demo.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class InkPresenter_Demo : UserControl, INotifyPropertyChanged
+    {
+
+        private Stroke LastStroke;
+        private Stack<Stroke> nextStrokes = new Stack<Stroke>();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public InkPresenter_Demo()
+        {
+            InitializeComponent();
+        }
+
+
+        private bool _canClearStrokes;
+        public bool CanClearStrokes
+        {
+            get { return _canClearStrokes; }
+            set { _canClearStrokes = value; OnPropertyChanged(); }
+        }
+
+        private bool _canUndoStroke;
+        public bool CanUndoStroke
+        {
+            get { return _canUndoStroke; }
+            set { _canUndoStroke = value; OnPropertyChanged(); }
+        }
+
+        private bool _canRedoStroke;
+        public bool CanRedoStroke
+        {
+            get { return _canRedoStroke; }
+            set { _canRedoStroke = value; OnPropertyChanged(); }
+        }
+
+
+
+        //A new stroke object named MyStroke is created. MyStroke is added to the StrokeCollection of the InkPresenter named MyIP
+        private void OnIP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            InkPad.CaptureMouse();
+            StylusPointCollection MyStylusPointCollection = new StylusPointCollection();
+            MyStylusPointCollection.Add(e.StylusDevice.GetStylusPoints(InkPad));
+            LastStroke = new Stroke(MyStylusPointCollection);
+            InkPad.Strokes.Add(LastStroke);
+            CanUndoStroke = true;
+            CanClearStrokes = true;
+        }
+
+        private void OnIP_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            InkPad.ReleaseMouseCapture();
+        }
+
+        //StylusPoint objects are collected from the MouseEventArgs and added to MyStroke. 
+        private void OnIP_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (LastStroke != null && InkPad.IsMouseCaptured)
+            {
+                LastStroke.StylusPoints.Add(e.StylusDevice.GetStylusPoints(InkPad));
+            }
+        }
+
+        //MyStroke is completed
+        private void OnIP_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void OnClearInkPad(object sender, RoutedEventArgs e)
+        {
+            LastStroke = null;
+            nextStrokes.Clear();
+            InkPad.Strokes.Clear();
+            CanClearStrokes = false;
+            CanUndoStroke = false;
+            CanRedoStroke = false;
+        }
+
+        private void OnUndoLastStroke(object sender, RoutedEventArgs e)
+        {
+            var strokes = InkPad.Strokes;
+            if(strokes.Count > 0)
+            {
+                nextStrokes.Push(strokes[strokes.Count - 1]);
+                strokes.RemoveAt(strokes.Count - 1);
+                CanRedoStroke = true;
+            }
+            if(strokes.Count == 0)
+            {
+                CanUndoStroke = false;
+            }
+        }
+
+        private void OnRedoLastStroke(object sender, RoutedEventArgs e)
+        {
+            if(nextStrokes.Count > 0)
+            {
+                InkPad.Strokes.Add(nextStrokes.Pop());
+            }
+            if(nextStrokes.Count == 0)
+            {
+                CanRedoStroke = false;
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ src/Samples/XAML_Controls/InkPresenter/InkPresenter_Demo.xaml.vb
@@ -0,0 +1,119 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Ink
+Imports System.Windows.Input
+
+Namespace OpenSilver.Samples.Showcase
+    Partial Public Class InkPresenter_Demo
+        Inherits UserControl
+        Implements INotifyPropertyChanged
+
+        Private LastStroke As Stroke
+        Private nextStrokes As Stack(Of Stroke) = New Stack(Of Stroke)()
+        'Public Event PropertyChanged As PropertyChangedEventHandler
+        Private Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private _canClearStrokes As Boolean
+
+        Public Property CanClearStrokes As Boolean
+            Get
+                Return _canClearStrokes
+            End Get
+            Set(ByVal value As Boolean)
+                _canClearStrokes = value
+                OnPropertyChanged()
+            End Set
+        End Property
+
+        Private _canUndoStroke As Boolean
+
+        Public Property CanUndoStroke As Boolean
+            Get
+                Return _canUndoStroke
+            End Get
+            Set(ByVal value As Boolean)
+                _canUndoStroke = value
+                OnPropertyChanged()
+            End Set
+        End Property
+
+        Private _canRedoStroke As Boolean
+
+        Public Property CanRedoStroke As Boolean
+            Get
+                Return _canRedoStroke
+            End Get
+            Set(ByVal value As Boolean)
+                _canRedoStroke = value
+                OnPropertyChanged()
+            End Set
+        End Property
+
+        Private Sub OnIP_MouseLeftButtonDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            InkPad.CaptureMouse()
+            Dim MyStylusPointCollection As StylusPointCollection = New StylusPointCollection()
+            MyStylusPointCollection.Add(e.StylusDevice.GetStylusPoints(InkPad))
+            LastStroke = New Stroke(MyStylusPointCollection)
+            InkPad.Strokes.Add(LastStroke)
+            CanUndoStroke = True
+            CanClearStrokes = True
+        End Sub
+
+        Private Sub OnIP_MouseLeftButtonUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            InkPad.ReleaseMouseCapture()
+        End Sub
+
+        Private Sub OnIP_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            If LastStroke IsNot Nothing AndAlso InkPad.IsMouseCaptured Then
+                LastStroke.StylusPoints.Add(e.StylusDevice.GetStylusPoints(InkPad))
+            End If
+        End Sub
+
+        Private Sub OnIP_LostMouseCapture(ByVal sender As Object, ByVal e As MouseEventArgs)
+        End Sub
+
+        Private Sub OnClearInkPad(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            LastStroke = Nothing
+            nextStrokes.Clear()
+            InkPad.Strokes.Clear()
+            CanClearStrokes = False
+            CanUndoStroke = False
+            CanRedoStroke = False
+        End Sub
+
+        Private Sub OnUndoLastStroke(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim strokes = InkPad.Strokes
+
+            If strokes.Count > 0 Then
+                nextStrokes.Push(strokes(strokes.Count - 1))
+                strokes.RemoveAt(strokes.Count - 1)
+                CanRedoStroke = True
+            End If
+
+            If strokes.Count = 0 Then
+                CanUndoStroke = False
+            End If
+        End Sub
+
+        Private Sub OnRedoLastStroke(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            If nextStrokes.Count > 0 Then
+                InkPad.Strokes.Add(nextStrokes.Pop())
+            End If
+            If nextStrokes.Count = 0 Then
+                CanRedoStroke = False
+            End If
+        End Sub
+
+        Private Sub OnPropertyChanged(
+<CallerMemberName> ByVal Optional propertyName As String = "")
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+    End Class
+End Namespace
+
