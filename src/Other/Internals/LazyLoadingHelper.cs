32c86d8de92c74b1d88e2318c36ffc449ec11001
roman-pikulenkor <roman@opensilver.net>
2024-06-17 08:38:55 -0400

    Add Blazor-Radzen DataGrid Demo

---
+++ src/Other/Internals/LazyLoadingHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public class LazyLoadingHelper
+    {
+        public static ILazyLoader AssemblyLoader;
+
+        public static Assembly GetLoadedAssembly(string assemblyName)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(a => a.GetName().Name == assemblyName);
+        }
+    }
+    public interface ILazyLoader
+    {
+        Task<IEnumerable<Assembly>> LoadAssembliesAsync(IEnumerable<string> assembliesToLoad);
+    }
+}
