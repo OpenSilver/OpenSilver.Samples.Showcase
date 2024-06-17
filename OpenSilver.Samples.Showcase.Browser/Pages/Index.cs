e1dd473fbaef90a802f4556fd2c0126608061879
roman-pikulenkor <roman@opensilver.net>
2024-06-17 08:38:55 -0400

    Add Blazor-Radzen DataGrid Demo

--- OpenSilver.Samples.Showcase.Browser/Pages/Index.cs
+++ OpenSilver.Samples.Showcase.Browser/Pages/Index.cs
@@ -5,12 +5,33 @@ using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
 using System;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace OpenSilver.Samples.Showcase.Browser.Pages
 {
     [Route("/")]
     public class Index : ComponentBase
     {
+        [Inject]
+        private LazyAssemblyLoader AssemblyLoader { get; set; }
+
+        private class LazyLoader : ILazyLoader
+        {
+            private LazyAssemblyLoader _assemblyLoader;
+
+            public LazyLoader(LazyAssemblyLoader lazyLoader)
+            {
+                _assemblyLoader = lazyLoader;
+            }
+
+            Task<IEnumerable<Assembly>> ILazyLoader.LoadAssembliesAsync(IEnumerable<string> assembliesToLoad)
+            {
+                return _assemblyLoader.LoadAssembliesAsync(assembliesToLoad);
+            }
+        }
+
         protected override void BuildRenderTree(RenderTreeBuilder __builder)
         {
         }
@@ -23,6 +44,8 @@ namespace OpenSilver.Samples.Showcase.Browser.Pages
             {
                 throw new InvalidOperationException("Failed to initialize OpenSilver. Check your browser's console for error details.");
             }
+
+            LazyLoadingHelper.AssemblyLoader = new LazyLoader(AssemblyLoader);
 
             Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
             Program.RunApplication();
