4449bc70d2f8853dbcc1e32a896893fc62914f9a
Sylvain <maillard.sy@gmail.com>
2025-03-05 17:23:20 +0100

    Add a couple Maui samples.

---
+++ src/Samples/Maui_Hybrid/Barometer/Barometer_Demo.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Devices.Sensors;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class Barometer_Demo : UserControl
+    {
+        public Barometer_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void BarometerToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                // Check the current location permission status.
+                var status = await Permissions.CheckStatusAsync<Permissions.Sensors>();
+
+                // If permission is not granted, request it.
+                if (status != PermissionStatus.Granted)
+                {
+                    status = await Permissions.RequestAsync<Permissions.Sensors>();
+                }
+
+                // If permission is granted, fetch the location.
+                if (status == PermissionStatus.Granted)
+                {
+                    if (Barometer.Default.IsSupported)
+                    {
+                        if (!Barometer.Default.IsMonitoring)
+                        {
+                            // Turn on Barometer
+                            Barometer.Default.ReadingChanged += Barometer_ReadingChanged;
+                            Barometer.Default.Start(SensorSpeed.UI);
+                            BarometerTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+                        }
+                        else
+                        {
+                            // Turn off Barometer
+                            Barometer.Default.Stop();
+                            Barometer.Default.ReadingChanged -= Barometer_ReadingChanged;
+                            BarometerTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+                        }
+                    }
+                    else
+                    {
+                        BarometerTextBlock.Text = $"The barometer is not supported on this device.";
+                    }
+                }
+            });
+        }
+
+        private void Barometer_ReadingChanged(object sender, BarometerChangedEventArgs e)
+        {
+            BarometerTextBlock.Text = $"Barometer: {e.Reading}";
+        }
+
+    }
+}
---
+++ src/Samples/Maui_Hybrid/Shake/Shake_Demo.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Devices.Sensors;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class Shake_Demo : UserControl
+    {
+        public Shake_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+    
+        int shakes = 0;
+        private void ShakeToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                // Check the current location permission status.
+                var status = await Permissions.CheckStatusAsync<Permissions.Sensors>();
+
+                // If permission is not granted, request it.
+                if (status != PermissionStatus.Granted)
+                {
+                    status = await Permissions.RequestAsync<Permissions.Sensors>();
+                }
+
+                // If permission is granted, fetch the location.
+                if (status == PermissionStatus.Granted)
+                {
+                    if (Accelerometer.Default.IsSupported)
+                    {
+                        if (!Accelerometer.Default.IsMonitoring)
+                        {
+                            // Turn on ShakeSensor
+                            Accelerometer.Default.ShakeDetected += ShakeSensor_ReadingChanged;
+                            Accelerometer.Default.Start(SensorSpeed.Game);
+                            ShakeTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+                        }
+                        else
+                        {
+                            // Turn off ShakeSensor
+                            Accelerometer.Default.Stop();
+                            Accelerometer.Default.ShakeDetected -= ShakeSensor_ReadingChanged;
+                            ShakeTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+                        }
+                    }
+                    else
+                    {
+                        ShakeTextBlock.Text = $"The ShakeSensor is not supported on this device.";
+                    }
+                }
+            });
+        }
+
+        private void ShakeSensor_ReadingChanged(object sender, EventArgs e)
+        {
+            ShakeTextBlock.Text = $"{++shakes} shakes detected.";
+        }
+    }
+}
