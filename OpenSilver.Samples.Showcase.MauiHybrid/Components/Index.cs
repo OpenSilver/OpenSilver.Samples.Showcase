77613fb8e94d332859ac75ad210f0a3d30b8b096
Andrei Arekhva <a.arekhva@gmail.com>
2025-03-14 16:54:22 +0300

    Allow to open external links for Mac/iOS

--- OpenSilver.Samples.Showcase.MauiHybrid/Components/Index.cs
+++ OpenSilver.Samples.Showcase.MauiHybrid/Components/Index.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using OpenSilver.MauiHybrid.Runner;
 
 namespace OpenSilver.Samples.Showcase.MauiHybrid.Components
@@ -9,12 +10,33 @@ namespace OpenSilver.Samples.Showcase.MauiHybrid.Components
         [Inject]
         private IMauiHybridRunner? Runner { get; set; }
 
+        [Inject]
+        public required IJSRuntime JS { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
 
             ArgumentNullException.ThrowIfNull(Runner);
             await Runner.RunApplicationAsync<Showcase.App>();
+
+#if MACCATALYST // workaround to prevent displaying text too bold on Mac
+            await JS.InvokeVoidAsync("eval", "document.body.style.fontSynthesis = 'none'");
+#endif
+            // seems like WebKit does not allow links with '_blank' target,
+            // so replace it with '_self' to be handled by BlazorWebView
+#if IOS || MACCATALYST
+            await JS.InvokeVoidAsync("eval",
+@"(function() {
+        const originalWindowOpen = window.open;
+        window.open = function(url, target, features) {
+            if (target === '_blank') {
+                target = '_self';
+            }
+            return originalWindowOpen(url, target, features);
+        };
+    })();");
+#endif
         }
     }
 }
--- OpenSilver.Samples.Showcase.MauiHybrid/MainPage.xaml.cs
+++ OpenSilver.Samples.Showcase.MauiHybrid/MainPage.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace OpenSilver.Samples.Showcase.MauiHybrid
+﻿using Microsoft.AspNetCore.Components.WebView;
+
+namespace OpenSilver.Samples.Showcase.MauiHybrid
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+#if IOS || MACCATALYST
+            blazorWebView.UrlLoading += OnBlazorWebViewUrlLoading;
+#endif
+        }
+
+        private async void OnBlazorWebViewUrlLoading(object? sender, UrlLoadingEventArgs e)
+        {
+            // if it is external link, WebKit does not handle it, so try to open it in Safari
+            if (e.UrlLoadingStrategy == UrlLoadingStrategy.OpenExternally)
+            {
+                await Launcher.TryOpenAsync(e.Url);
+            }
         }
     }
 }
