623a87cef660b752b39bc3bee1b4fc6867d0499b
Andrei Arekhva <a.arekhva@gmail.com>
2025-03-14 15:44:15 +0300

    Prevent overlapping the status bar on iOS

--- OpenSilver.Samples.Showcase.MauiHybrid/App.xaml.cs
+++ OpenSilver.Samples.Showcase.MauiHybrid/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿namespace OpenSilver.Samples.Showcase.MauiHybrid
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace OpenSilver.Samples.Showcase.MauiHybrid
 {
-    public partial class App : Application
+    public partial class App : Microsoft.Maui.Controls.Application
     {
         public App()
         {
@@ -9,7 +12,9 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "OpenSilver Showcase" };
+            var mainPage = new MainPage();
+            mainPage.On<iOS>().SetUseSafeArea(true); // prevent overlapping the status bar
+            return new Window(mainPage) { Title = "OpenSilver Showcase" };
         }
     }
 }
