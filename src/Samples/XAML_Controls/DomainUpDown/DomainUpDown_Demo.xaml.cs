f8915c455ed1e9ae95c043997d648f5127756ba7
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-01 16:37:19 +0300

    Add DomainUpDown sample

---
+++ src/Samples/XAML_Controls/DomainUpDown/DomainUpDown_Demo.xaml.cs
@@ -0,0 +1,13 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase;
+
+[SearchKeywords("input", "updown", "spinner", "counter", "control", "buttonspinner")]
+public partial class DomainUpDown_Demo : ContentControl
+{
+    public DomainUpDown_Demo()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Samples/XAML_Controls/DomainUpDown/DomainUpDown_Demo.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+
+    <SearchKeywords("input", "updown", "spinner", "counter", "control", "buttonspinner")>
+    Partial Public Class DomainUpDown_Demo
+        Inherits ContentControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+
+End Namespace
