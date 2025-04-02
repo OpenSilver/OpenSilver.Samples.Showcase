8598f1fc73b0a15ca6fd1c77f202c3aec00422e2
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-02 15:24:35 +0300

    Add Rating, improve NumericUpDown

---
+++ src/Samples/XAML_Controls/Rating/Rating_Demo.xaml.cs
@@ -0,0 +1,13 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase;
+
+[SearchKeywords("input", "stars", "control")]
+public partial class Rating_Demo : ContentControl
+{
+    public Rating_Demo()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Samples/XAML_Controls/Rating/Rating_Demo.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+
+    <SearchKeywords("input", "stars", "control")>
+    Partial Public Class Rating_Demo
+        Inherits ContentControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+
+End Namespace
