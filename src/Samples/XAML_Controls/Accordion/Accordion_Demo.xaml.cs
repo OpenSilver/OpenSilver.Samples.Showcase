49580eb4478a90669a44182d81bac25c87914e84
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-03 13:14:10 +0300

    Add Accordion demo

---
+++ src/Samples/XAML_Controls/Accordion/Accordion_Demo.xaml.cs
@@ -0,0 +1,13 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase;
+
+[SearchKeywords("items", "expander", "list")]
+public partial class Accordion_Demo : ContentControl
+{
+    public Accordion_Demo()
+    {
+        InitializeComponent();
+    }
+}
