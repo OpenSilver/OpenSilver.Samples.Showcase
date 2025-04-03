eb4fbe164bd984dc62bae24fab950dedc52e3815
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-03 14:54:37 +0300

    Add LayoutTransformer

---
+++ src/Samples/XAML_Controls/LayoutTransformer/LayoutTransformer_Demo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase;
+
+[SearchKeywords("transformation", "scale", "rotate", "skew")]
+public partial class LayoutTransformer_Demo : ContentControl
+{
+    public LayoutTransformer_Demo()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        layoutTransformer.ApplyLayoutTransform();
+    }
+}
---
+++ src/Samples/XAML_Controls/LayoutTransformer/LayoutTransformer_Demo.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+
+    <SearchKeywords("transformation")>
+    Partial Public Class LayoutTransformer_Demo
+        Inherits ContentControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub OnSliderValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+            layoutTransformer.ApplyLayoutTransform()
+        End Sub
+
+    End Class
+
+End Namespace
