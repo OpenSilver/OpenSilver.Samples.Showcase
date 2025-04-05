6079f8c5992312afe3ca14516c44c031974af1e6
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-05 12:11:23 +0300

    Imrove navigation, show Frame demo

--- src/Samples/SearchPage.xaml.cs
+++ src/Samples/SearchPage.xaml.cs
@@ -3,11 +3,14 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 namespace OpenSilver.Samples.Showcase
 {
-    public partial class SearchPage : UserControl
+    public partial class SearchPage : Page
     {
+        private const string SearchArgName = "SearchTerms";
+
         public SearchPage()
         {
             InitializeComponent();
@@ -16,39 +19,37 @@ namespace OpenSilver.Samples.Showcase
             SearchField.AddHandler(KeyDownEvent, new KeyEventHandler(SearchField_KeyDown), true);
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (NavigationContext.QueryString.TryGetValue(SearchArgName, out var searchTerms) && !string.IsNullOrWhiteSpace(searchTerms))
+            {
+                SearchField.Text = searchTerms;
+                PerformSearch(searchTerms);
+            }
+        }
+
         private void OnSearchFieldLoaded(object sender, RoutedEventArgs e)
         {
             SearchField.Focus();
-
-            //get the MainPage and start the search:
-            if (Application.Current.RootVisual is MainPage mainPage)
-            {
-                string originalPath = mainPage.PageContainer.CurrentSource.OriginalString;
-                if (originalPath.Length > 8)
-                {
-                    string searchTerms = originalPath.Substring(8); // get rid of the "/Search/" from "Search/searchParameters"
-                    if (!string.IsNullOrWhiteSpace(searchTerms))
-                    {
-                        SearchField.Text = searchTerms;
-                        PerformSearch(searchTerms);
-                    }
-                }
-            }
         }
 
         private void ButtonSearch_Click(object sender, RoutedEventArgs e)
         {
-            string searchText = SearchField.Text;
-            PerformSearch(searchText);
+            NavigateToSearch();
         }
 
         private void SearchField_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                string searchText = SearchField.Text;
-                PerformSearch(searchText);
+                NavigateToSearch();
             }
+        }
+
+        private void NavigateToSearch()
+        {
+            string searchText = SearchField.Text;
+            NavigationService.Navigate(new Uri($"/Search/{searchText}", UriKind.Relative));
         }
 
         internal void PerformSearch(string searchText)
