0c1925232c0f9b713263981dfa98024b86c52d3c
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-09 11:24:24 +0300

    Add HeaderedContentControl demo

---
+++ src/Samples/XAML_Controls/HeaderedContentControl/HeaderedContentControl_Demo.xaml.fs
@@ -0,0 +1,9 @@
+﻿namespace OpenSilver.Samples.Showcase
+
+open OpenSilver.Samples.Showcase.Search
+
+[<SearchKeywords("container")>]
+type HeaderedContentControl_Demo() as this =
+    inherit HeaderedContentControl_DemoXaml()
+    do
+        this.InitializeComponent()
