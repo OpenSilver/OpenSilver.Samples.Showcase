aab224be2b96aa78050af2956a40167af1324c9f
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-09 12:32:01 +0300

    Add GlobalCalendar demo

---
+++ src/Samples/XAML_Controls/GlobalCalendar/GlobalCalendar_Demo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase;
+
+[SearchKeywords("input", "date", "selection", "schedule", "picker", "control", "culture", "globalization")]
+public partial class GlobalCalendar_Demo : UserControl
+{
+    public GlobalCalendar_Demo()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCultureChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (culturesCombo.SelectedItem is not ComboBoxItem selected)
+        {
+            return;
+        }
+
+        var culture = new CultureInfo(selected.Tag as string);
+        globalCalendar.CalendarInfo = new CultureCalendarInfo(culture);
+    }
+}
