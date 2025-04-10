35cbbcc6d26e8532097bc63cb5849adb3160a94c
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-10 14:08:57 +0300

    Add Themes demo

---
+++ src/Samples/XAML_Features/Themes/Themes_Demo.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+    <SearchKeywords("styling", "styles", "theme", "customization", "UI", "modern")>
+    Partial Public Class Themes_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
