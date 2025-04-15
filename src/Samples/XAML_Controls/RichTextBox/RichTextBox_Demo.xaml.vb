de2e596adbc82b2fc0a8e88a2eae6c6aa906d4a5
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-15 16:53:36 +0300

    Add RichTextBlock and RichTextBox demos

---
+++ src/Samples/XAML_Controls/RichTextBox/RichTextBox_Demo.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+
+    <SearchKeywords("input", "text", "entry")>
+    Partial Public Class RichTextBox_Demo
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+
+End Namespace
