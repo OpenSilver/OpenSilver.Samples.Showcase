d983619a6be8a851219b1f1f7636ef0630a60781
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-17 14:10:55 +0300

    Add PWA card

---
+++ src/Samples/Interop_Samples/PWA/PWA_Demo.xaml.fs
@@ -0,0 +1,8 @@
+﻿namespace OpenSilver.Samples.Showcase
+
+open OpenSilver.Samples.Showcase.Search
+
+[<SearchKeywords("progressive", "web", "application", "desktop", "offline")>]
+type PWA_Demo() as this =
+    inherit PWA_DemoXaml()
+    do this.InitializeComponent()
