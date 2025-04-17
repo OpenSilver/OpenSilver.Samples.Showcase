0e2f4498e930b501dec75b3adf1d60bad51d4efc
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-17 15:37:40 +0300

    Fix base path for service worker

--- OpenSilver.Samples.Showcase.Browser/wwwroot/service-worker.published.js
+++ OpenSilver.Samples.Showcase.Browser/wwwroot/service-worker.published.js
@@ -12,7 +12,7 @@ const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
-const base = "/";
+const base = "/opensilvershowcase/release/";
 const baseUrl = new URL(base, self.origin);
 const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).href);
 
