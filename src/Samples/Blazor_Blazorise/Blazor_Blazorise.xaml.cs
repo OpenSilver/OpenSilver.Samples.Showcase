13ee132021e7b57578746db7b2e22c272badcccf
Sylvain <maillard.sy@gmail.com>
2025-04-18 18:17:14 +0200

    Add Blazorise with some rough samples

--- src/Samples/Blazor_Blazorise/Blazor_Blazorise.xaml.cs
+++ src/Samples/Blazor_Blazorise/Blazor_Blazorise.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -10,13 +11,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Navigation;
 
-namespace OpenSilver.Samples.Showcase.Samples.Blazor_Blazorise
+namespace OpenSilver.Samples.Showcase
 {
     public partial class Blazor_Blazorise : UserControl
     {
         public Blazor_Blazorise()
         {
             this.InitializeComponent();
+
+            LoadContent();
+        }
+
+        private void LoadContent()
+        {
+            try
+            {
+                Assembly assemblySample = AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "OpenSilver.Samples.Showcase.Blazorise");
+                Type type = assemblySample.GetType("OpenSilver.Samples.Showcase.Blazorise_Sample");
+                object content = Activator.CreateInstance(type);
+                Content = (UIElement)content;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new TargetInvocationException(ex);
+            }
         }
     }
 }
