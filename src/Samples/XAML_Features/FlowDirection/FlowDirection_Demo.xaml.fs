a77c65323325c01cd926221639d697d5cfed5788
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-22 16:52:13 +0300

    Add FlowDirection demo

---
+++ src/Samples/XAML_Features/FlowDirection/FlowDirection_Demo.xaml.fs
@@ -0,0 +1,10 @@
+namespace OpenSilver.Samples.Showcase
+
+open OpenSilver.Samples.Showcase.Search
+
+[<SearchKeywords("RTL", "right to left", "flow", "direction", "layout", "arabic", "hebrew", "localization", "internationalization")>]
+type FlowDirection_Demo() as this =
+    inherit FlowDirection_DemoXaml()
+
+    do
+        this.InitializeComponent()
