0392b35d62eccb06504e309fa2ab532f2570a104
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-25 15:57:30 +0300

    Add Alwan color picker demo

---
+++ src/Samples/JS_Libs/ColorPicker/ColorPicker.cs
@@ -0,0 +1,79 @@
+using CSHTML5.Native.Html.Controls;
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace OpenSilver.Samples.Showcase;
+
+public class ColorPicker : HtmlPresenter
+{
+    private static bool _isJsLibLoaded;
+
+    private object _domElement;
+
+    #region Color
+    public Color Color
+    {
+        get => (Color)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+
+    public static readonly DependencyProperty ColorProperty =
+        DependencyProperty.Register(nameof(Color), typeof(Color), typeof(ColorPicker), new PropertyMetadata(Colors.Black));
+    #endregion
+
+    public ColorPicker()
+    {
+        Loaded += OnLoaded;
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Loaded -= OnLoaded;
+
+        await LoadJSLibrary();
+        _domElement = Interop.GetDiv(this);
+
+        Html = "<div></div>";
+        Interop.ExecuteJavaScriptVoidAsync($@"
+          const alwan = new Alwan($0.firstChild.firstChild, {{
+            theme: 'dark',
+            toggle: false,
+            popover: false,
+            color: {JsHexColor},
+            margin: 0,
+            format: 'rgb',
+            inputs: {{hex: true, rgb: true,  hsl: true}},
+            singleInput: true,
+          }});
+          alwan.on('color', (ev) => {{
+            $1(ev.r, ev.g, ev.b, ev.a);
+          }});
+          alwan.on('change', (ev) => {{
+            $1(ev.r, ev.g, ev.b, ev.a);
+          }});
+          $0.alwan = alwan;
+        ", _domElement, (Action<byte, byte, byte, float>)OnColorChangedInAlwan);
+    }
+
+    private void OnColorChangedInAlwan(byte r, byte g, byte b, float a)
+    {
+        Color = Color.FromArgb((byte)Math.Round(a * byte.MaxValue), r, g, b);
+    }
+
+    private string JsHexColor => $"'#{GetHex(Color.R)}{GetHex(Color.G)}{GetHex(Color.B)}{GetHex(Color.A)}'";
+
+    private string GetHex(byte number) => number.ToString("X2", CultureInfo.InvariantCulture);
+
+    private static async Task LoadJSLibrary()
+    {
+        if (!_isJsLibLoaded)
+        {
+            await Interop.LoadCssFile("https://cdn.jsdelivr.net/npm/alwan/dist/css/alwan.min.css");
+            await Interop.LoadJavaScriptFile("https://cdn.jsdelivr.net/npm/alwan/dist/js/alwan.min.js");
+            _isJsLibLoaded = true;
+        }
+    }
+}
