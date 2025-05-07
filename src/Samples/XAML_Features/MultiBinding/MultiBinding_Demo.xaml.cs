22f0e0178f8f62b0049de0f8cad2d6f26d4ffd6e
Andrey Arekhva <a.arekhva@gmail.com>
2025-05-07 09:37:16 +0300

    Add search keywords and converter source code to MultiBinding demo

--- src/Samples/XAML_Features/MultiBinding/MultiBinding_Demo.xaml.cs
+++ src/Samples/XAML_Features/MultiBinding/MultiBinding_Demo.xaml.cs
@@ -1,7 +1,9 @@
+using OpenSilver.Samples.Showcase.Search;
 using System.Windows.Controls;
 
 namespace OpenSilver.Samples.Showcase;
 
+[SearchKeywords("binding", "MVVM", "converter")]
 public partial class MultiBinding_Demo : UserControl
 {
     public MultiBinding_Demo()
