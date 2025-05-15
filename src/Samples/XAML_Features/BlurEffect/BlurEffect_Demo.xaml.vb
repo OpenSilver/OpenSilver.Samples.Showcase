71d798849ab657819dc5e1f1644366bbd67080fc
Andrey Arekhva <a.arekhva@gmail.com>
2025-05-15 17:02:39 +0300

    Add BlurEffect demo

---
+++ src/Samples/XAML_Features/BlurEffect/BlurEffect_Demo.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows.Controls
+Imports OpenSilver.Samples.Showcase.Search
+
+Namespace OpenSilver.Samples.Showcase
+    <SearchKeywords("graphics", "blur", "effect", "filter", "UI", "visual", "image")>
+    Partial Public Class BlurEffect_Demo
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
