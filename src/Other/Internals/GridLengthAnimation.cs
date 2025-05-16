0d31f35280d17a5c269b91b832cbc80e739a0853
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2025-05-16 10:17:50 +0200

    Implement GridLengthAnimation (work in progress)

---
+++ src/Other/Internals/GridLengthAnimation.cs
@@ -0,0 +1,176 @@
+﻿/*===================================================================================
+* 
+*   Copyright (c) Userware/OpenSilver.net
+*      
+*   This file is part of OpenSilver (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*   
+*   IMPORTANT: Make sure to preserve this copyright notice on all copies of this code.
+*  
+\*====================================================================================*/
+
+
+using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace OpenSilver.Samples.Showcase;
+
+/// <summary>
+/// Animates the value of a <see cref="GridLength"/> property between two target values using linear interpolation over a specified <see cref="Timeline.Duration"/>.
+/// </summary>
+public sealed class GridLengthAnimation : AnimationTimeline
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridLengthAnimation"/> class.
+    /// </summary>
+    public GridLengthAnimation() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridLengthAnimation"/> class that animates to the specified value over the specified duration.
+    /// The starting value for the animation is the base value of the property being animated or the output from another animation.
+    /// </summary>
+    /// <param name="toValue">
+    /// The destination value of the animation.
+    /// </param>
+    /// <param name="duration">
+    /// The length of time the animation takes to play from start to finish, once. See the <see cref="Timeline.Duration"/> property for more information.
+    /// </param>
+    public GridLengthAnimation(GridLength toValue, Duration duration)
+    {
+        To = toValue;
+        Duration = duration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridLengthAnimation"/> class that animates to the specified value over the specified duration 
+    /// and has the specified fill behavior. The starting value for the animation is the base value of the property being animated or the output 
+    /// from another animation.
+    /// </summary>
+    /// <param name="toValue">
+    /// The destination value of the animation.
+    /// </param>
+    /// <param name="duration">
+    /// The length of time the animation takes to play from start to finish, once. See the <see cref="Timeline.Duration"/> property for more information.
+    /// </param>
+    /// <param name="fillBehavior">
+    /// Specifies how the animation behaves when it is not active.
+    /// </param>
+    public GridLengthAnimation(GridLength toValue, Duration duration, FillBehavior fillBehavior)
+    {
+        To = toValue;
+        Duration = duration;
+        FillBehavior = fillBehavior;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridLengthAnimation"/> class that animates from the specified starting value to the specified 
+    /// destination value over the specified duration.
+    /// </summary>
+    /// <param name="fromValue">
+    /// The starting value of the animation.
+    /// </param>
+    /// <param name="toValue">
+    /// The destination value of the animation.
+    /// </param>
+    /// <param name="duration">
+    /// The length of time the animation takes to play from start to finish, once. See the <see cref="Timeline.Duration"/> property for more information.
+    /// </param>
+    public GridLengthAnimation(GridLength fromValue, GridLength toValue, Duration duration)
+    {
+        From = fromValue;
+        To = toValue;
+        Duration = duration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GridLengthAnimation"/> class that animates from the specified starting value to the specified
+    /// destination value over the specified duration and has the specified fill behavior.
+    /// </summary>
+    /// <param name="fromValue">
+    /// The starting value of the animation.
+    /// </param>
+    /// <param name="toValue">
+    /// The destination value of the animation.
+    /// </param>
+    /// <param name="duration">
+    /// The length of time the animation takes to play from start to finish, once. See the <see cref="Timeline.Duration"/> property for more information.
+    /// </param>
+    /// <param name="fillBehavior">
+    /// Specifies how the animation behaves when it is not active.
+    /// </param>
+    public GridLengthAnimation(GridLength fromValue, GridLength toValue, Duration duration, FillBehavior fillBehavior)
+    {
+        From = fromValue;
+        To = toValue;
+        Duration = duration;
+        FillBehavior = fillBehavior;
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="EasingFunction"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty EasingFunctionProperty =
+        DependencyProperty.Register(
+            nameof(EasingFunction),
+            typeof(IEasingFunction),
+            typeof(GridLengthAnimation),
+            new PropertyMetadata((object)null));
+
+    /// <summary>
+    /// Gets or sets the easing function applied to this animation.
+    /// </summary>
+    /// <returns>
+    /// The easing function applied to this animation.
+    /// </returns>
+    public IEasingFunction EasingFunction
+    {
+        get => (IEasingFunction)GetValue(EasingFunctionProperty);
+        set => SetValue(EasingFunctionProperty, value);
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="From"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty FromProperty =
+        DependencyProperty.Register(
+            nameof(From),
+            typeof(GridLength?),
+            typeof(GridLengthAnimation),
+            new PropertyMetadata((object)null));
+
+    /// <summary>
+    /// Gets or sets the animation's starting value.
+    /// </summary>
+    /// <returns>
+    /// The starting value of the animation. The default value is null.
+    /// </returns>
+    public GridLength? From
+    {
+        get => (GridLength?)GetValue(FromProperty);
+        set => SetValue(FromProperty, value);
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="To"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty ToProperty =
+        DependencyProperty.Register(
+            nameof(To),
+            typeof(GridLength?),
+            typeof(GridLengthAnimation),
+            new PropertyMetadata((object)null));
+
+    /// <summary>
+    /// Gets or sets the animation's ending value.
+    /// </summary>
+    /// <returns>
+    /// The ending value of the animation. The default value is null.
+    /// </returns>
+    public GridLength? To
+    {
+        get => (GridLength?)GetValue(ToProperty);
+        set => SetValue(ToProperty, value);
+    }
+
+}
