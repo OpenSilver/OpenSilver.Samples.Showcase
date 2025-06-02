936a20f375ae6176e1ee514b5f450f990951c34e
Andrey Arekhva <a.arekhva@gmail.com>
2025-06-02 15:25:10 +0300

    Fix scaling issues on Android if font scale in phone settings is greater than 100%

--- OpenSilver.Samples.Showcase.MauiHybrid/Platforms/Android/AndroidWebViewHandler.cs
+++ OpenSilver.Samples.Showcase.MauiHybrid/Platforms/Android/AndroidWebViewHandler.cs
@@ -7,6 +7,7 @@ public class AndroidWebViewHandler : BlazorWebViewHandler
     protected override void ConnectHandler(global::Android.Webkit.WebView webView)
     {
         webView.Settings.SetSupportMultipleWindows(false);
+        webView.Settings.TextZoom = 100; // Set text zoom to 100% to avoid scaling issues
 
         base.ConnectHandler(webView);
     }
