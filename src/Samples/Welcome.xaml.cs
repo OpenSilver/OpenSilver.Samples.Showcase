c90b157c380e0ec20afc73dbef0c39ed0cdfab35
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-06-12 15:14:46 +0600

    Introduce a separate description for the iOS version.

--- src/Samples/Welcome.xaml.cs
+++ src/Samples/Welcome.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Maui.Devices;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace OpenSilver.Samples.Showcase
 {
@@ -7,6 +9,13 @@ namespace OpenSilver.Samples.Showcase
         public Welcome()
         {
             InitializeComponent();
+
+            //Apple review team does not like the default description, so we decided to introduce a separate one
+            if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+            {
+                DescriptionDefault.Visibility = Visibility.Collapsed;
+                DescriptionIos.Visibility = Visibility.Visible;
+            }
         }
     }
 }
