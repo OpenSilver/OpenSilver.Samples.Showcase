a3a81e255ec26634aa85e4ea342f24ceff15182d
Andrey Arekhva <a.arekhva@gmail.com>
2025-06-18 18:25:54 +0300

    Implement loading more demos when the user scrolls to the bottom

--- src/Other/Internals/ItemsControlLoadingBehavior.cs
+++ src/Other/Internals/ItemsControlLoadingBehavior.cs
@@ -2,8 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media;
-using System.Windows.Media.Effects;
 
 namespace OpenSilver.Samples.Showcase
 {
@@ -41,6 +39,48 @@ namespace OpenSilver.Samples.Showcase
                 null
             );
 
+        public static void ShowLoadingPopup(this ItemsControl itemsControl)
+        {
+            // create & show
+            var popup = new Popup
+            {
+                Child = new LoadingControl(),
+                Placement = PlacementMode.Absolute,
+                IsHitTestVisible = false,
+                IsOpen = false
+            };
+
+            /*
+            // size & position to overlay the ItemsControl
+            var root = Application.Current.RootVisual as FrameworkElement;
+            var transform = itemsControl.TransformToVisual(root);
+            var topLeft = transform.Transform(new Point(0, 0));
+            popup.HorizontalOffset = topLeft.X;
+            popup.VerticalOffset = topLeft.Y;
+            popup.Width = itemsControl.ActualWidth;
+            popup.Height = itemsControl.ActualHeight;
+            */
+
+            // size & position to overlay the whole window
+            var host = (FrameworkElement)Application.Current.RootVisual;
+            popup.Width = host.ActualWidth;
+            popup.Height = host.ActualHeight;
+
+            itemsControl.SetValue(LoadingPopupProperty, popup);
+            popup.IsOpen = true;
+        }
+
+        public static void HideLoadingPopup(this ItemsControl itemsControl)
+        {
+            // close & cleanup
+            var popup = (Popup)itemsControl.GetValue(LoadingPopupProperty);
+            if (popup != null)
+            {
+                popup.IsOpen = false;
+                itemsControl.ClearValue(LoadingPopupProperty);
+            }
+        }
+
         private static void OnShowLoadingOnGeneratingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (!(d is ItemsControl itemsControl))
@@ -65,12 +105,7 @@ namespace OpenSilver.Samples.Showcase
                 }
 
                 // ensure any popup is closed
-                var existing = (Popup)itemsControl.GetValue(LoadingPopupProperty);
-                if (existing != null)
-                {
-                    existing.IsOpen = false;
-                    itemsControl.ClearValue(LoadingPopupProperty);
-                }
+                HideLoadingPopup(itemsControl);
             }
         }
 
@@ -80,43 +115,11 @@ namespace OpenSilver.Samples.Showcase
 
             if (status == GeneratorStatus.GeneratingContainers)
             {
-                // create & show
-                var popup = new Popup
-                {
-                    Child = new LoadingControl(),
-                    Placement = PlacementMode.Absolute,
-                    IsHitTestVisible = false,
-                    IsOpen = false
-                };
-
-                /*
-                // size & position to overlay the ItemsControl
-                var root = Application.Current.RootVisual as FrameworkElement;
-                var transform = itemsControl.TransformToVisual(root);
-                var topLeft = transform.Transform(new Point(0, 0));
-                popup.HorizontalOffset = topLeft.X;
-                popup.VerticalOffset = topLeft.Y;
-                popup.Width = itemsControl.ActualWidth;
-                popup.Height = itemsControl.ActualHeight;
-                */
-
-                // size & position to overlay the whole window
-                var host = (FrameworkElement)Application.Current.RootVisual;
-                popup.Width = host.ActualWidth;
-                popup.Height = host.ActualHeight;
-
-                itemsControl.SetValue(LoadingPopupProperty, popup);
-                popup.IsOpen = true;
+                ShowLoadingPopup(itemsControl);
             }
             else
             {
-                // close & cleanup
-                var popup = (Popup)itemsControl.GetValue(LoadingPopupProperty);
-                if (popup != null)
-                {
-                    popup.IsOpen = false;
-                    itemsControl.ClearValue(LoadingPopupProperty);
-                }
+                HideLoadingPopup(itemsControl);
             }
         }
     }
