56ae6274c9d4de86ba5fb1e89a379def2296add6
Sylvain <maillard.sy@gmail.com>
2025-07-25 17:58:49 +0200

    Add a Syncfusion DataGrid sample

---
+++ OpenSilver.Samples.Showcase.Syncfusion/Samples/DataGrid/SyncfusionDataGrid_Demo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class SyncfusionDataGrid_Demo : UserControl
+    {
+        public SyncfusionDataGrid_Demo()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new SyncfusionModel.EmployeesData();
+
+        }
+    }
+}
