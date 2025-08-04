e2fb041ce97236fdf480a0bbfa14d3b6a520e6e3
Sylvain <maillard.sy@gmail.com>
2025-08-04 16:28:53 +0200

    Add GeoBlazor project (no content yet for a more informative next commit)

---
+++ OpenSilver.Samples.Showcase.GeoBlazor/Samples/GeoBlazor_Sample.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class GeoBlazor_Sample : UserControl
+    {
+        public GeoBlazor_Sample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Samples/Blazor_GeoBlazor/Blazor_GeoBlazor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public partial class Blazor_GeoBlazor : UserControl
+    {
+        public Blazor_GeoBlazor()
+        {
+            this.InitializeComponent();
+
+            LoadContent();
+        }
+
+        private void LoadContent()
+        {
+            try
+            {
+                Content = new GeoBlazor_Sample();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new TargetInvocationException(ex);
+            }
+        }
+    }
+}
