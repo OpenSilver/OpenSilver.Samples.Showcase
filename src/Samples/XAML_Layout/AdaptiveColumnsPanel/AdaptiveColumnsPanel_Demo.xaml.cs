f2d999bf6988ad7fa7f7c595c75016e7b512ed36
Sylvain <maillard.sy@gmail.com>
2025-10-07 18:21:51 +0200

    Add samples for ControlsKit controls + move AnimatedNavigationBar to Layout folder/Page

---
+++ src/Samples/XAML_Layout/AdaptiveColumnsPanel/AdaptiveColumnsPanel_Demo.xaml.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Samples.Showcase.Search;
+using System.Windows.Controls;
+
+namespace OpenSilver.Samples.Showcase
+{
+    [SearchKeywords("layout", "adaptive", "Panel", "container", "UI")]
+    public partial class AdaptiveColumnsPanel_Demo : UserControl
+    {
+        public AdaptiveColumnsPanel_Demo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Samples/XAML_Layout/StaggeredPanel/StaggeredPanel_Demo.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using OpenSilver.Samples.Showcase.Search;
+
+namespace OpenSilver.Samples.Showcase
+{
+[SearchKeywords("layout", "Panel", "container", "UI")]
+    public partial class StaggeredPanel_Demo : UserControl
+    {
+        public StaggeredPanel_Demo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
